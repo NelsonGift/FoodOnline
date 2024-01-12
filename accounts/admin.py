bfe8720c6c58bb970bc682f3a3bc4386c75f67e9
Sabit Nelson Gift <sabitnelson@gmail.com>
2024-01-12 16:19:42 +0300

    Register user functionality and homepaage setup

---
+++ accounts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import User,UserProfile
+from django.contrib.auth.admin import UserAdmin
+# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('role','phone_number')
+    filter_horizontal =()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserProfile)
